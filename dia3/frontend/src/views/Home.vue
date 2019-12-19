e3cb15d12d71ac24f568a9e1c1832a900e1c630d
Rwan <rwan.souza@hotmail.com>
2019-12-19 17:39:43 -0300

    Trabalho dia 4

---
+++ dia3/frontend/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1> Ola</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'home'
+ 
+}
+</script>
